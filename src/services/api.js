128f01fa364820ba185e82a64b98559810bb142c
Guilherme De Jesus <gui-rock02@hotmail.com>
2024-02-20 20:46:11 -0300

    responsividade consertada.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
+    //baseURL: "http://localhost:3333", //local environment
     baseURL: "https://foodexplorer-maxtr.onrender.com",
     withCredentials: true
 })
